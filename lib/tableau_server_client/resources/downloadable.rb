3892e8f11aa7f15234055285398c68d65aa21cf8
Shimpei Kodama <shimpei-kodama@cookpad.com>
2019-05-20 18:18:55 +0900

    Cache downloaded content for better performance

--- lib/tableau_server_client/resources/downloadable.rb
+++ lib/tableau_server_client/resources/downloadable.rb
@@ -4,20 +4,22 @@ module TableauServerClient
       module Downloadable
 
         def download(file_path: nil)
+          return @content_body if @content_body and (file_path.nil? or @file_path == file_path)
+          @file_path = file_path
           response = client.download(location(query_params: {"includeExtract": "False"}), file_path: file_path)
-          type, disposition = response.headers.values_at('content-type', 'content-disposition')
-          case type
+          content_type = response.headers['content-type']
+          case content_type
           when 'application/xml'
-            return Nokogiri::XML(response.body)
+            return @content_body = Nokogiri::XML(response.body)
           when 'application/octet-stream'
             Zip::InputStream.open(StringIO.new(response.body)) do |io|
               while entry = io.get_next_entry
-                return Nokogiri::XML(io.read) if entry.name =~ /.*\.(tds|twb)/
+                return @content_body = Nokogiri::XML(io.read) if entry.name =~ /.*\.(tds|twb)/
               end
               raise "TDS or TWB file not found for: #{location.path}"
             end
           else
-            raise "Unknown content-type: #{type}"
+            raise "Unknown content-type: #{content_type}"
           end
         end
     end
